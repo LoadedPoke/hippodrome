42b5e3b6ccb88ecb7cf70dfc6d28b57828ca1ff6
loadedpoke <loadedpoke@gmail.com>
2025-02-03 13:45:08 +0200

    Add constructorNameNullMessage method

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class HorseTest {
@@ -7,5 +8,14 @@ class HorseTest {
     @Test
     void constructorNameNullException() {
         assertThrows(IllegalArgumentException.class, () -> new Horse(null, 0, 0));
+    }
+
+    @Test
+    void constructorNameNullMessage() {
+        try {
+            new Horse(null, 0, 0);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Name cannot be null.", e.getMessage());
+        }
     }
 }
