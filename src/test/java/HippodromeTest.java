7d7f99da6592fcf3cae77f02759cb3484bcc1ea1
loadedpoke <loadedpoke@gmail.com>
2025-02-04 01:16:48 +0200

    Create HippodromeTest, add constructorArgumentNullException

---
+++ src/test/java/HippodromeTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HippodromeTest {
+
+    @Test
+    void constructorArgumentNullException() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+    }
+
+    @Test
+    void getHorses() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void getWinner() {
+    }
+}
