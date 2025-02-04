ed528345ee9d7886d9f70204924b098c2c2fc71b
loadedpoke <loadedpoke@gmail.com>
2025-02-04 12:08:29 +0200

    Add move

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class HippodromeTest {
 
@@ -47,6 +49,16 @@ class HippodromeTest {
 
     @Test
     void move() {
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            Horse mockHorse = mock(Horse.class);
+            horses.add(mockHorse);
+        }
+        Hippodrome hippodrome = new Hippodrome(horses);
+        hippodrome.move();
+        for (Horse horse : horses) {
+            verify(horse).move();
+        }
     }
 
     @Test
